e3f46e907555c93dd4b4de67b419dd9671ca8491
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2020-12-18 22:39:07 -0500

    commit tree 64f9a0e170cf814b9a25e9d801d6dd5e8ca76052

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,25 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    low_key = nil
+    low_value = Float::INFINITY
+    name_hash.each do |key, value|
+        if value < low_value
+            low_value = value
+            low_key = key
+        end
+    end
+    low_key
 end
+
+
+# smallest hash value
+#   does not call the `#keys` method
+#   does not call the `#values` method
+#   does not call the `#min` method
+#   does not call the `#sort` method
+#   does not call the `#sort_by` method
+#   does not call the `#min_by` method
+#   returns the key of the smallest hash value
+#   returns the key of the smallest hash value example 2
+#   returns nil for an empty hash
